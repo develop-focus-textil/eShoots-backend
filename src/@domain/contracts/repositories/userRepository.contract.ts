604b40d2282a42be7fea9e9fe9515b15c6c85227
christiangsn <christianguimaraes1996@gmail.com>
2023-03-11 15:23:02 -0300

    feat: [domain] - add method updatedGoogleID in contracts UserRepository #15

--- src/@domain/contracts/repositories/userRepository.contract.ts
+++ src/@domain/contracts/repositories/userRepository.contract.ts
@@ -1,4 +1,6 @@
 import { type UserAggregate } from '@domain/user'
 import { type IGenericRepositoryContract } from '@infra/shared/core/contracts/genericContractRepository'
 
-export type IUserRepositoryContract = IGenericRepositoryContract<UserAggregate>
+export type IUserRepositoryContract = IGenericRepositoryContract<UserAggregate> & {
+  updatedGoogleID: (id: string, googleID: string) => Promise<void>
+}
