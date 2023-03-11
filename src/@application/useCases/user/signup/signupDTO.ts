5e5ab8988272dc036c6afa89871c842fb92c2dbd
christiangsn <christianguimaraes1996@gmail.com>
2023-03-11 02:37:19 -0300

    feat: [application] - insert DTO types system, google and facebook in signUpUseCase #3

--- src/@application/useCases/user/signup/signupDTO.ts
+++ src/@application/useCases/user/signup/signupDTO.ts
@@ -1,14 +1,28 @@
 import { type OS } from '@domain/user'
 
 export interface ISignUpDTO {
-  email: string
-  password: string
-  ip: string
-  acceptedTerms: boolean
-  userAgent: {
-    name: string
-    version: string
-    os: keyof typeof OS
-    type: string
+  system?: ISignUpDTO.system
+  google?: ISignUpDTO.google
+  facebook?: ISignUpDTO.facebook
+}
+
+export namespace ISignUpDTO {
+  export type system = {
+    email: string
+    password: string
+    ip: string
+    acceptedTerms: boolean
+    userAgent: {
+      name: string
+      version: string
+      os: keyof typeof OS
+      type: string
+    }
+  }
+  export type google = {
+    token: string
+  }
+  export type facebook = {
+    token: string
   }
 }
